ae8b0915af854cd2505de65ef2f413c89f995d55
Shivam Thapliyal <shivamthapliyal571@gmail.com>
2025-01-20 19:50:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://rirm-assignment-frontend.vercel.app"],
     methods: ["GET", "POST"],
   })
 );
